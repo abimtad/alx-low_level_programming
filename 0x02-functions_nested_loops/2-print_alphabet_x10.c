dbe7694cdc5ef6d6777b200d27edb8e50b8d3db9
Abel Tadesse <tadesseabel26@gmail.com>
2023-12-19 12:52:47 -0800

    fix and new updates

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - prints alphabet
+* print_alphabet_10x - prints alphabet 10 times
 * Return: Always 0 (Success)
 *
 */
@@ -11,11 +11,11 @@ void print_alphabet_x10(void)
 	int c;
 
 	for (c = 0; c <= 10; c++)
-	{ 
+	{
 		for (lt = 'a'; lt <= 'z'; lt++)
 		{
 			_putchar(lt);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Read me sis not litt

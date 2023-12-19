c7c3f8a78e8062a250adef7dbd5d589fe45a67f0
Abel Tadesse <tadesseabel26@gmail.com>
2023-12-19 13:42:33 -0800

    bugs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 				printf("\n");
 				break;
 			}
-			printf("%d", n);
+			printf("%d, ", n);
 		}
 	}
 	else
@@ -32,7 +32,7 @@ void print_to_98(int n)
 				printf("\n");
 				break;
 			}
-			printf("%d", n);
+			printf("%d, ", n);
 		}
 	}
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,16 +6,18 @@
 * Return: 1 for positive num, -1 for negative or 0 for anything else
 *
 */
+
 int _abs(int n)
 {
 	if (n < 0)
 	{
 		int v;
+
 		v = n * -1;
 		return (v);
 	}
 	else
 	{
-		return (0);
+		return (n);
 	}
 }
